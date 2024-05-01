325cde59476c22a9da7018ce8ae871c0e6d9ea98
Kwonyoungoh <rnjsduddh05@gmail.com>
2024-05-02 00:02:28 +0900

    보안그룹추가

--- main.tf
+++ main.tf
@@ -108,7 +108,7 @@ resource "aws_route_table_association" "private" {
 }
 
 #####################################
-# ecs 생성
+# ECS 생성
 # SSM 연결을 위한 IAM 인스턴스 프로파일
 data "aws_iam_instance_profile" "ssm" {
   name = "AmazonSSMRoleForInstancesQuickSetup"
@@ -132,11 +132,37 @@ data "aws_ami" "amazon_linux_2023" {
   }
 }
 
+# ECS 인스턴스 보안 그룹
+resource "aws_security_group" "ecs_instance_sg" {
+  name        = "ecs-instance-security-group"
+  description = "Allow traffic from ALB to ECS instances"
+  vpc_id      = aws_vpc.yotest_vpc.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ecs-instance-sg"
+  }
+}
+
 resource "aws_launch_configuration" "yotest" {
     name = "ecs-launch-configuration"
     image_id = data.aws_ami.amazon_linux_2023.image_id
     instance_type = "t3.small"
     iam_instance_profile = data.aws_iam_instance_profile.ssm.name
+    security_groups = [aws_security_group.ecs_instance_sg.id]
 
     # ECS 에이전트 설정
     user_data = <<-EOF
@@ -163,4 +189,140 @@ resource "aws_autoscaling_group" "yotest_asg" {
         value               = "ECS Instance - yotest"
         propagate_at_launch = true
     }
+}
+
+#####################################
+# ALB
+# ALB 보안 그룹 설정
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-security-group"
+  description = "Allow HTTPS traffic to ALB"
+  vpc_id      = aws_vpc.yotest_vpc.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
+
+# Application Load Balancer 생성
+resource "aws_lb" "yotest_alb" {
+  name               = "yotest-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [for s in aws_subnet.public : s.id]
+
+  tags = {
+    Name = "yotest-ALB"
+  }
+}
+
+# HTTPS 리스너 설정
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.yotest_alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = data.aws_acm_certificate.yotest.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.yotest_tg.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "yotest" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.yotest_tg.arn
+  }
+
+  condition {
+    host_header {
+      values = ["yotest.link", "www.yotest.link"]
+    }
+  }
+}
+
+# 타겟그룹 생성
+resource "aws_lb_target_group" "yotest_tg" {
+  name     = "yotest-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.yotest_vpc.id
+
+  health_check {
+    interval            = 30
+    path                = "/"
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 2
+    protocol            = "HTTP"
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "yotest-tg"
+  }
+}
+
+#####################################
+# RDS
+resource "aws_db_instance" "yotest_rds" {
+  allocated_storage    = 10
+  db_name              = "yotestdb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = var.rds_username
+  password             = var.rds_secret
+  parameter_group_name = "default.mysql5.7"
+  db_subnet_group_name = aws_db_subnet_group.yotest_rds.name
+  vpc_security_group_ids = [aws_security_group.yotest_rds.id]
+
+  publicly_accessible = false 
+  skip_final_snapshot  = true
+}
+
+resource "aws_db_subnet_group" "yotest_rds" {
+  name       = "my-subnet-group"
+  subnet_ids = [for s in aws_subnet.private : s.id]
+}
+
+resource "aws_security_group" "yotest_rds" {
+  name        = "rds-public-access"
+  description = "Allow only inbound traffic from ECS security group on port 3306"
+  vpc_id      = data.aws_vpc.default.id 
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.ecs_instance_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
--- provider.tf
+++ provider.tf
@@ -3,3 +3,10 @@ provider "aws" {
   access_key = var.iam_access_key
   secret_key = var.iam_secret_key
 }
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.iam_access_key
+  secret_key = var.iam_secret_key
+  alias = "us-east-1"
+}
